e84a6645c3f166d81ba1ae47e5f1a03abad91c59
Tanvir <Tanvirislamnyc@gmail.com>
2024-12-28 01:14:51 -0500

    completed

--- frontend/src/ContactForm.jsx
+++ frontend/src/ContactForm.jsx
@@ -1,65 +1,69 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-const ContactForm = () => {
-  const [firstName, setFirstName] = useState('')
-  const [lastName, setLastName] = useState('')
-  const [email, setEmail] = useState('')
+const ContactForm = ({ existingContact = {}, updateCallback }) => {
+    const [firstName, setFirstName] = useState(existingContact.firstName || "");
+    const [lastName, setLastName] = useState(existingContact.lastName || "");
+    const [email, setEmail] = useState(existingContact.email || "");
 
-  const onSubmit = async (e) => {
-    e.preventDefault()
+    const updating = Object.entries(existingContact).length !== 0
 
-    const data = {
-      firstName,
-      lastName,
-      email
+    const onSubmit = async (e) => {
+        e.preventDefault()
+
+        const data = {
+            firstName,
+            lastName,
+            email
+        }
+        const url = "http://127.0.0.1:5000/" + (updating ? `update_contact/${existingContact.id}` : "create_contact")
+        const options = {
+            method: updating ? "PATCH" : "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        }
+        const response = await fetch(url, options)
+        if (response.status !== 201 && response.status !== 200) {
+            const data = await response.json()
+            alert(data.message)
+        } else {
+            updateCallback()
+        }
     }
 
-    const url = "http://127.0.0.1:5000/create_contact"
-    const options = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(data)
-    }
-    const response = await fetch(url, options)
-    if(response.status !== 201 && response.status !== 200){
-      const data = await response.json()
-      alert(data.message)
-    } else {
-      // success
-    }
-  }
-
-  return (
-    <form onSubmit={onSubmit}>
-      <div>
-        <label>First Name:</label>
-        <input
-          type="text"
-          value={firstName}
-          onChange={(e) => setFirstName(e.target.value)}
-        />
-      </div>
-      <div>
-        <label>Last Name:</label>
-        <input
-          type="text"
-          value={lastName}
-          onChange={(e) => setLastName(e.target.value)}
-        />
-      </div>
-      <div>
-        <label>Email:</label>
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </div>
-      <button type="submit">Submit</button>
-    </form>
-  )
-}
+    return (
+        <form onSubmit={onSubmit}>
+            <div>
+                <label htmlFor="firstName">First Name:</label>
+                <input
+                    type="text"
+                    id="firstName"
+                    value={firstName}
+                    onChange={(e) => setFirstName(e.target.value)}
+                />
+            </div>
+            <div>
+                <label htmlFor="lastName">Last Name:</label>
+                <input
+                    type="text"
+                    id="lastName"
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
+                />
+            </div>
+            <div>
+                <label htmlFor="email">Email:</label>
+                <input
+                    type="text"
+                    id="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+            </div>
+            <button type="submit">{updating ? "Update" : "Create"}</button>
+        </form>
+    );
+};
 
 export default ContactForm
